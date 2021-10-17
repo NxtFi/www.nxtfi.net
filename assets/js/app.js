50475791f97eb814a98d204b5c2cf3cf25f99674
Elvis Bonilla <elvisdbl17@gmail.com>
2021-10-17 13:48:40 -0400

    layz load

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@
 
 (() => {
   const header = document.querySelector("#header");
+
+  const sliders = document.querySelectorAll(".slide-in");
+
   window.onscroll = function () {
     var top = window.scrollY;
     if (top >= 50) {
@@ -10,4 +13,28 @@
       header.classList.remove("active");
     }
   };
+
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px",
+  };
+
+  const appearOnScroll = new IntersectionObserver(function (
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+
+  sliders.forEach((slider) => {
+    appearOnScroll.observe(slider);
+  });
 })();
